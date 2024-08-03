0c81b7ddebba7c97a7b86133a2c3b5e033a3c3fb
MarkTBSS <marktbss@gmail.com>
2024-08-03 10:33:20 +0700

    [ADD] First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Arrow Functions
+
+1. Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+
+# Run
+
+```
+node script.js
+node assignment.js
+```
---
+++ assignment.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+console.log(percPortugal3, percChina3, percUSA3);
